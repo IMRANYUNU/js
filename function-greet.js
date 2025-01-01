16d03422c32ea640a52a447ce9bd4408b58d0eba
Imran Yunus <imranyunus.blk@gmail.com>
2025-01-01 11:27:09 +0530

    same f

---
+++ function-greet.js
@@ -0,0 +1,5 @@
+function greet(name){
+    console.log(`hello ${name}`);
+    console.log('hvae a nise day today!');
+}
+greet('sohail');
---
+++ function-odd-even.js
@@ -0,0 +1,10 @@
+function calcoddeven(n){
+    let remainder = n % 2;
+    if(remainder===0){
+        console.log("even");
+    }
+    else {
+        console.log("odd")
+    }
+}
+calcoddeven(56);
